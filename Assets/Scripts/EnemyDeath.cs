ec14e51b7d157f9295cffc3e25785e340c25184d
Maximilian Klimko <82115185+Mamigako@users.noreply.github.com>
2024-12-05 14:09:18 +0000

    Enemy death script created

---
+++ Assets/Scripts/EnemyDeath.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour {
+
+    public EnemyMovement enemy;
+
+void OnTriggerEnter2D(Collider2D collision)
+{
+    if (collision.CompareTag("Bullet"))
+    {
+        // Disable the SpriteRenderer to make the enemy invisible
+        GetComponent<SpriteRenderer>().enabled = false;
+
+        // Disable the Collider to prevent further collisions
+        GetComponent<Collider2D>().enabled = false;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+}
