98ad72f0853fcf7065356b67a4556b5dfa7db6b5
Frillion <fridriksoebech@gmail.com>
2024-12-05 18:18:44 +0000

    [QoL]:Added an end screen to the alpha

---
+++ Assets/Scripts/System/LevelEnd.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public Animator levelEndAnimator;
+    public TarodevController.PlayerController playerControlls;
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.CompareTag("Player")){
+            levelEndAnimator.SetTrigger("LevelFinished");
+            playerControlls.enabled = false;
+        }
+    }
+}
