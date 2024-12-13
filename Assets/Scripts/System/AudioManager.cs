9d5d80b28438cfbdeac8d27f228f4212a65b0368
Danielzhx <danielzhrdn@gmail.com>
2024-12-13 13:09:32 +0000

    Added an audio manager

---
+++ Assets/Scripts/System/AudioManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace AudioManger
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager instance;
+        AudioSource audioSource;
+        public AudioClip coinSound;
+
+        void Awake()
+        {
+            instance = this;
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlayCoinSound()
+        {
+            audioSource.PlayOneShot(coinSound);
+        }
+    }
+}
