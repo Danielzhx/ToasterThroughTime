7e49a4202e5725e72009b7ff7cf9f96d72969940
Danielzhx <danielzhrdn@gmail.com>
2024-12-14 21:43:23 +0000

    Implemented the pause menu (the return to game button is still not working)

---
+++ Assets/Scripts/System/PauseMenu.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("References")]
+    public GameObject pauseMenuPanel;
+
+    private bool isPaused = false;
+
+    void Start()
+    {
+        pauseMenuPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+                return;
+            }
+            else
+            {
+                PauseGame();
+                return;
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        isPaused = true;
+        pauseMenuPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
